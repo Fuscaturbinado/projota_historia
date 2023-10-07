c64a58f07a92aaca18af01c610147a1e71faf126
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2023-10-06 21:22:40 -0300

    atualização

--- trabalho.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.caixa1').onmouseover = function() {
-    var mensagem = document.createElement('caixa'); 
-    mensagem.textContent = 'hitler-a-carreir'; 
-    document.body.appendChild(mensagem);
-  }
